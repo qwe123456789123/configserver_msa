ec2b64288a5b7c24aa2f990fb6481f886c1f12cc
qwe123456789123 <p1199019@naver.com>
2025-01-13 09:56:18 +0900

    초기업로드

---
+++ src/main/java/com/koreait/Configserver2Application.java
@@ -0,0 +1,13 @@
+package com.koreait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configserver2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configserver2Application.class, args);
+	}
+
+}
